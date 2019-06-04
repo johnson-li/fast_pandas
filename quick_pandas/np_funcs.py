6f0d81d4474b9fc7564a9bcb23d9f3dc9b6ac8f7
lixuebing <johnsonli1993@gmail.com>
2019-06-04 14:49:23 +0800

    fix

--- quick_pandas/np_funcs.py
+++ quick_pandas/np_funcs.py
@@ -7,4 +7,4 @@ NP_FUNCS_SUM = 1
 NP_FUNCS_MEDIAN = 2
 
 NP_FUNCS_MAP = dict(zip(range(len(NP_FUNCS)), NP_FUNCS))
-NP_FUNCS_MAP_REVERSE = {val: key for key, val in NP_FUNCS_MAP}
+NP_FUNCS_MAP_REVERSE = {val: key for key, val in NP_FUNCS_MAP.items()}
