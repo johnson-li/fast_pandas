cb4bfe4ed5882a30bf8b9c1fd93c5e5be8efaf25
lixuebing <johnsonli1993@gmail.com>
2019-06-13 17:39:36 +0800

    bump version 0.1.4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ URL = 'https://github.com/johnson-li/fast-pandas'
 EMAIL = 'johnsonli1993@gmail.com'
 AUTHOR = 'Johnson Li'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
